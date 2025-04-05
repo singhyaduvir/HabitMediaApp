0b88019b6042c27a5a47cbb6208da14f773eb43f
Yaduvir Singh <singhyaduvir2006@gmail.com>
2025-04-05 16:27:20 -0400

    Initial Commit

---
+++ HabitMedia/HabitMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMediaApp.swift
+//  HabitMedia
+//
+//  Created by Zscaler Test on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitMediaTests/HabitMediaTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMediaTests.swift
+//  HabitMediaTests
+//
+//  Created by Zscaler Test on 4/5/25.
+//
+
+import Testing
+@testable import HabitMedia
+
+struct HabitMediaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
